df8b0e8cae9dc4f0e6b3041e02fc08a13e63e57c
Nikita Bakumenko <n.bakumenko@numedy.com>
2024-08-02 14:59:26 +0300

    feat: add placeholders for appointement page

--- src/pages/DoctorAppointmentPage/ui/DoctorAppointmentPage.tsx
+++ src/pages/DoctorAppointmentPage/ui/DoctorAppointmentPage.tsx
@@ -1,7 +1,42 @@
 export const DoctorAppointmentPage = () => {
   return (
-    <div>
-      DoctorAppointmentPage
-    </div>
+    <form>
+      <label>
+        <p>Выберите тип услуги</p>
+        <select>
+          <option>Тип услуги 1</option>
+          <option>Тип услуги 2</option>
+          <option>Тип услуги 3</option>
+        </select>
+      </label>
+      <label>
+        <p>Выберите услугу</p>
+        <select>
+          <option>Услуга 1</option>
+          <option>Услуга 2</option>
+          <option>Услуга 3</option>
+        </select>
+      </label>
+      <label>
+        <p>Выберите Врача</p>
+        <select>
+          <option>Врач 1</option>
+          <option>Врач 2</option>
+          <option>Врач 3</option>
+        </select>
+      </label>
+      <label>
+        <p>Ваше полное ФИО</p>
+        <input type="text"/>
+      </label>
+      <label>
+        <p>Ваша адресс</p>
+        <input type="text"/>
+      </label>
+      <label>
+        <p>Ваш телефон</p>
+        <input type="text"/>
+      </label>
+    </form>
   )
 };
