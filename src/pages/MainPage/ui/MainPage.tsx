629c290f95868dd815068703831344573e0f9a1e
Nikita Bakumenko <n.bakumenko@numedy.com>
2024-08-02 13:48:59 +0300

    feat: add main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,29 @@
+import s from './MainPage.module.scss'
+
 export const MainPage = () => {
   return (
-    <div>
-      MainPage
+    <div className={s.pageWrapper}>
+      <div className={s.logo}>
+        <img src={'public/medical-icon.svg'} alt={'Логотип'}/>
+        <h1>МедЭксперт</h1>
+      </div>
+      <div className={s.select}>
+        <button>
+          Записаться
+        </button>
+        <button>
+          Предложение/Жалобы
+        </button>
+      </div>
+      <div className={s.call_wrapper}>
+        <span>
+          Позвонить нам:
+        </span>
+        <a href={'tel:880033300800'}>
+          <img src={'public/phone-icon.svg'} alt={'Логотип'}/>
+          8-800-333-0-800
+        </a>
+      </div>
     </div>
   )
 }
